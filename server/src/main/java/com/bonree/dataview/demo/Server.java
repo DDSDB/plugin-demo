0c0d2622c16a8b2f6e05aea2ea23af950aebed44
tangdq@bonree.com <tangdq@bonree.com>
2022-02-10 18:14:29 +0800

    remove unneeded file and add a plugin for test

--- server/src/main/java/com/bonree/dataview/demo/Server.java
+++ server/src/main/java/com/bonree/dataview/demo/Server.java
@@ -9,9 +9,9 @@ import java.util.Set;
 public class Server {
     public static void main(String[] args) {
         PluginConfig config = new PluginConfig();
-        config.setPluginDir("D:\\project\\test\\plugins\\plugin");
+        config.setPluginDir("D:\\project\\plugin-demo");
 //        config.setPluginDir("D:\\project\\out-plugin");
-        config.setLoadList(Lists.newArrayList("target"));
+        config.setLoadList(Lists.newArrayList("test-plugin"));
         Set<Greeting> greetings = PluginInitializer.loadPlugins(Greeting.class, config);
         for (Greeting greeting : greetings) {
             System.out.println(greeting.getGreeting());
